105b1e01fae81a7aa2c8d3364c134d61f8a7d7d2
gberseth <gberseth@Ragnorok>
2016-02-13 18:41:40 -0800

    Adding MonteCarloSearch.

---
+++ MonteCarloTreeSearch.py
@@ -0,0 +1,187 @@
+"""
+    Used to do some game traversal
+
+"""
+
+from MapGame import *
+from math import *
+import random
+
+
+class MapGameWrapper:
+    """ A state of the game Nim. In Nim, players alternately take 1,2 or 3 chips with the 
+        winner being the player to take the last chip. 
+        In Nim any initial state of the form 4n+k for k = 1,2,3 is a win for player 1
+        (by choosing k) chips.
+        Any initial state of the form 4n is a win for player 2.
+    """
+    def __init__(self):
+        map = loadMap()
+        self._game = Map(map)
+        self.playerJustMoved = 2 # At the root pretend the player just moved is p2 - p1 has the first move
+        
+    def Clone(self):
+        """ Create a deep clone of this game state.
+        """
+        st = MapGameWrapper()
+        st.playerJustMoved = self.playerJustMoved
+        st._game.setState(self._game.getState())
+        return st
+
+    def DoMove(self, move):
+        """ Update a state by carrying out the given move.
+            Must update playerJustMoved.
+        """
+        assert move >= 0 and move <= 7 and move == int(move)
+        self._game.act(move)
+        self.playerJustMoved = 3 - self.playerJustMoved
+        # print "Location: " + str(self._game.getState())
+        
+    def GetMoves(self):
+        """ Get all possible moves from this state.
+        """
+        if self._game.reachedTarget():
+           return [] 
+        return range(0,8)
+    
+    def GetResult(self, playerjm):
+        """ Get the game result from the viewpoint of playerjm. 
+        """
+        assert self._game.reachedTarget()
+        print "Reached Goal:"
+        if self.playerJustMoved == playerjm:
+            return 1.0 # playerjm took the last chip and has won
+        else:
+            return 0.0 # playerjm's opponent took the last chip and has won
+
+    def __repr__(self):
+        s = "Location:" + str(self._game.getState()) + " JustPlayed:" + str(self.playerJustMoved)
+        return s
+
+    
+    
+
+class Node:
+    """ A node in the game tree. Note wins is always from the viewpoint of playerJustMoved.
+        Crashes if state not specified.
+    """
+    def __init__(self, move = None, parent = None, state = None):
+        self.move = move # the move that got us to this node - "None" for the root node
+        self.parentNode = parent # "None" for the root node
+        self.childNodes = []
+        self.wins = 0
+        self.visits = 0
+        self.untriedMoves = state.GetMoves() # future child nodes
+        self.playerJustMoved = state.playerJustMoved # the only part of the state that the Node needs later
+        
+    def UCTSelectChild(self):
+        """ Use the UCB1 formula to select a child node. Often a constant UCTK is applied so we have
+            lambda c: c.wins/c.visits + UCTK * sqrt(2*log(self.visits)/c.visits to vary the amount of
+            exploration versus exploitation.
+        """
+        s = sorted(self.childNodes, key = lambda c: c.wins/c.visits + sqrt(2*log(self.visits)/c.visits))[-1]
+        return s
+    
+    def AddChild(self, m, s):
+        """ Remove m from untriedMoves and add a new child node for this move.
+            Return the added child node
+        """
+        n = Node(move = m, parent = self, state = s)
+        self.untriedMoves.remove(m)
+        self.childNodes.append(n)
+        return n
+    
+    def Update(self, result):
+        """ Update this node - one additional visit and result additional wins. result must be from the viewpoint of playerJustmoved.
+        """
+        self.visits += 1
+        self.wins += result
+
+    def __repr__(self):
+        return "[M:" + str(self.move) + " W/V:" + str(self.wins) + "/" + str(self.visits) + " U:" + str(self.untriedMoves) + "]"
+
+    def TreeToString(self, indent):
+        s = self.IndentString(indent) + str(self)
+        for c in self.childNodes:
+             s += c.TreeToString(indent+1)
+        return s
+
+    def IndentString(self,indent):
+        s = "\n"
+        for i in range (1,indent+1):
+            s += "| "
+        return s
+
+    def ChildrenToString(self):
+        s = ""
+        for c in self.childNodes:
+             s += str(c) + "\n"
+        return s
+
+
+def UCT(rootstate, itermax, verbose = False):
+    """ Conduct a UCT search for itermax iterations starting from rootstate.
+        Return the best move from the rootstate.
+        Assumes 2 alternating players (player 1 starts), with game results in the range [0.0, 1.0]."""
+
+    rootnode = Node(state = rootstate)
+
+    for i in range(itermax):
+        node = rootnode
+        state = rootstate.Clone()
+
+        # Select
+        while node.untriedMoves == [] and node.childNodes != []: # node is fully expanded and non-terminal
+            node = node.UCTSelectChild()
+            state.DoMove(node.move)
+
+        # Expand
+        if node.untriedMoves != []: # if we can expand (i.e. state/node is non-terminal)
+            m = random.choice(node.untriedMoves) 
+            state.DoMove(m)
+            node = node.AddChild(m,state) # add child and descend tree
+
+        # Rollout - this can often be made orders of magnitude quicker using a state.GetRandomMove() function
+        while state.GetMoves() != []: # while state is non-terminal
+            state.DoMove(random.choice(state.GetMoves()))
+
+        # Backpropagate
+        while node != None: # backpropagate from the expanded node and work back to the root node
+            node.Update(state.GetResult(node.playerJustMoved)) # state is terminal. Update node with result from POV of node.playerJustMoved
+            node = node.parentNode
+
+    # Output some information about the tree - can be omitted
+    if (verbose): print rootnode.TreeToString(0)
+    else: print rootnode.ChildrenToString()
+
+    return sorted(rootnode.childNodes, key = lambda c: c.visits)[-1].move # return the move that was most visited
+                
+def UCTPlayGame():
+    """ Play a sample game between two UCT players where each player gets a different number 
+        of UCT iterations (= simulations = tree nodes).
+    """
+    state = MapGameWrapper() # uncomment to play Othello on a square board of the given size
+    # state = OXOState() # uncomment to play OXO
+    # state = NimState(15) # uncomment to play Nim with the given number of starting chips
+    while (state.GetMoves() != []):
+        print str(state)
+        if state.playerJustMoved == 1:
+            m = UCT(rootstate = state, itermax = 1000, verbose = False) # play with values for itermax and verbose = True
+        else:
+            m = UCT(rootstate = state, itermax = 100, verbose = False)
+        print "Best Move: " + str(m) + "\n"
+        state.DoMove(m)
+    if state.GetResult(state.playerJustMoved) == 1.0:
+        print "Player " + str(state.playerJustMoved) + " wins!"
+    elif state.GetResult(state.playerJustMoved) == 0.0:
+        print "Player " + str(3 - state.playerJustMoved) + " wins!"
+    else: print "Nobody wins!"
+
+if __name__ == "__main__":
+    """ Play a single game to the end using UCT for both players. 
+    """
+    UCTPlayGame()
+
+            
+                          
+            
