7483f8db67a4c03c02bd8513a9b30cad6f754c5b
Neo-X <glen@fracturedplane.com>
2016-03-07 09:50:54 -0800

    Creating new game.

--- BallGame.py
+++ BallGame.py
@@ -110,68 +110,72 @@ class ParticleBox:
         return True
 
 
-#------------------------------------------------------------
-# set up initial state
-np.random.seed(0)
-init_state = -0.5 + np.random.random((50, 4))
-init_state[:, :2] *= 3.9
+class BallGame(object):
 
-init_state = [[-2,0,1,0]]
+    def __init__(self):
+        #------------------------------------------------------------
+        # set up initial state
+        np.random.seed(0)
+        init_state = -0.5 + np.random.random((50, 4))
+        init_state[:, :2] *= 3.9
+        
+        init_state = [[-2,0,1,0]]
+        
+        self._box = ParticleBox(init_state, size=0.04)
+        self._dt = 1. / 60 # 30fps
+        
+        
+        #------------------------------------------------------------
+        # set up figure and animation
+        self._fig = plt.figure()
+        self._fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
+        self._ax = self._fig.add_subplot(111, aspect='equal', autoscale_on=False,
+                             xlim=(-3.2, 3.2), ylim=(-2.4, 2.4))
+        
+        # particles holds the locations of the particles
+        self._particles, = self._ax.plot([], [], 'bo', ms=6)
+        
+        # rect is the box edge
+        self._rect = plt.Rectangle(self._box.bounds[::2],
+                             self._box.bounds[1] - self._box.bounds[0],
+                             self._box.bounds[3] - self._box.bounds[2],
+                             ec='none', lw=2, fc='none')
+        self._ax.add_patch(self._rect)
 
-box = ParticleBox(init_state, size=0.04)
-dt = 1. / 60 # 30fps
-
-
-#------------------------------------------------------------
-# set up figure and animation
-fig = plt.figure()
-fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
-ax = fig.add_subplot(111, aspect='equal', autoscale_on=False,
-                     xlim=(-3.2, 3.2), ylim=(-2.4, 2.4))
-
-# particles holds the locations of the particles
-particles, = ax.plot([], [], 'bo', ms=6)
-
-# rect is the box edge
-rect = plt.Rectangle(box.bounds[::2],
-                     box.bounds[1] - box.bounds[0],
-                     box.bounds[3] - box.bounds[2],
-                     ec='none', lw=2, fc='none')
-ax.add_patch(rect)
-
-def init():
-    """initialize animation"""
-    global box, rect
-    particles.set_data([], [])
-    rect.set_edgecolor('none')
-    return particles, rect
-
-def animate(i):
-    """perform animation step"""
-    global box, rect, dt, ax, fig
-    out = box.step(dt)
-
-    ms = int(fig.dpi * 2 * box.size * fig.get_figwidth()
-             / np.diff(ax.get_xbound())[0])
+    def init(self):
+        """initialize animation"""
+        self._particles.set_data([], [])
+        self._rect.set_edgecolor('none')
+        plt.ion()
+        plt.show()
+        return self._particles, self._rect
     
-    # update pieces of the animation
-    rect.set_edgecolor('k')
-    particles.set_data(box.state[:, 0], box.state[:, 1])
-    particles.set_markersize(ms)
-    # return particles, rect
-    return out
-
+    def animate(self, i):
+        """perform animation step"""
+        out = self._box.step(self._dt)
     
-def act(action):
-    run = True
-    print "Acting"
-    box.state[0][3] = action[1]
-    for i in range(500):
-        run = animate(i)
-        print box.state
-        fig.canvas.draw()
-        if not run:
-            return False
+        ms = int(self._fig.dpi * 2 * self._box.size * self._fig.get_figwidth()
+                 / np.diff(self._ax.get_xbound())[0])
+        
+        # update pieces of the animation
+        self._rect.set_edgecolor('k')
+        self._particles.set_data(self._box.state[:, 0], self._box.state[:, 1])
+        self._particles.set_markersize(ms)
+        # return particles, rect
+        return out
+    
+        
+    def act(self, action):
+        run = True
+        print "Acting: " + str(action)
+        self._box.state[0][3] = action[1]
+        self._box.state[0][2] = action[0]
+        for i in range(500):
+            run = self.animate(i)
+            # print box.state
+            self._fig.canvas.draw()
+            if not run:
+                return False
 
 #ani = animation.FuncAnimation(fig, animate, frames=600,
 #                               interval=10, blit=True, init_func=init)
@@ -186,9 +190,9 @@ def act(action):
 
 # plt.show()
 
-plt.ion()
-plt.show()
-init()
+ballGame = BallGame()
+
+ballGame.init()
 
 for i in range(10):
-    act([0,1])
+    ballGame.act([1,1])
